4295448a6f90d6847eb26083a035d7a38e086213
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-04 17:58:42 +0200

    feat: add /requests GET and POST endpoints for retrieving and submitting prompts implement request tracking with IP-based storage and Gemini integration add lifespan event to create database tables on startup chore: add database models, engine, session and CRUD functions for ChatRequests feat: implement SQLite storage with SQLAlchemy for chat request history

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-from fastapi import FastAPI, Body
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI, Body, Request
+
+from db import Base, engine, get_user_requests, add_request_data
 from gemini_client import get_answer_from_gemini
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(engine)
+    print("All tables are created")
+    yield
+
+app = FastAPI(
+    title="EX",
+    lifespan=lifespan
+)
 
 @app.get("/requests")
-def get_my_requests():
-    return "yoo!"
+def get_my_requests(request: Request):
+    user_ip_address = request.client.host
+    print(f"{user_ip_address = }")
+    user_requests = get_user_requests(ip_address=user_ip_address)
+    return user_requests
 
 @app.post("/requests")
 def send_prompt(
+        request: Request,
         prompt: str = Body(embed=True)
 ):
+    user_ip_address = request.client.host
     answer = get_answer_from_gemini(prompt)
+    add_request_data(
+        ip_address=user_ip_address,
+        prompt=prompt,
+        response=answer,
+    )
     return {"answer": answer}
